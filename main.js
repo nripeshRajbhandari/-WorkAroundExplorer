e0b94ffb27bd52465683f106db952dae4dc53fa4
Nripesh Rajbhandari <nripesh.rajbhandari@gmail.com>
2024-04-20 19:33:17 +0100

    functions created as per the radio click and calculation functions created accordingly.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // TODO: Add your import statements here.
-import {getRoles,getCompanies} from'./modules/salaryData.js';
-import salaryData from './modules/salaryData.js';
+import salaryData, {getRoles,getCompanies} from './modules/salaryData.js';
 
+import {getAverageSalaryByRole,getAverageSalaryByCompany,getSalaryAtCompany,getIndustryAverageSalary} from './modules/workAroundModule.js';
+
+// import formatNumber from './modules/utilities.js';
 
 // TODO: Get the companies and roles using the salaryData module.
 const companies = getCompanies();
@@ -56,10 +58,10 @@ function updateResults(){
   if (!company || !role) { return; }
 
   // TODO: Use the workAroundModule functions to calculate the needed data.
-  const averageSalaryByRole = 0;
-  const averageSalaryByCompany = 0;
-  const salary = 0;
-  const industryAverageSalary = 0;
+  const averageSalaryByRole = getAverageSalaryByRole(role);
+  const averageSalaryByCompany = getAverageSalaryByCompany(company);
+  const salary = getSalaryAtCompany(role,company);
+  const industryAverageSalary = getIndustryAverageSalary();
 
   // Render them to the screen.
   document.getElementById('salarySelected').innerText = `The salary for ${role}s at ${company} is \$${salary}`;
